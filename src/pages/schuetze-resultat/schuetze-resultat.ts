c6faa3e16573975e1cf1c4f7914bae9e444efed1
martinfrancois <fracoisma@hotmail.com>
2018-06-03 11:42:55 +0200

    added poc for select of result

--- src/pages/schuetze-resultat/schuetze-resultat.ts
+++ src/pages/schuetze-resultat/schuetze-resultat.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Stich} from "../../app/entities/Stich";
 
 /**
  * Generated class for the SchuetzeResultatPage page.
@@ -16,13 +17,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SchuetzeResultatPage {
 
   schuetze: string;
+  resultate: number[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.schuetze = navParams.get('schuetze');
+    this.getResultat()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResultatPage');
+  }
+
+  getResultat() { // TODO: for testing only
+    let resultatMax = 80;
+    if (resultatMax % 10 == 0) {
+      // schüsse sind jeweils ganzzahlig
+      return Array(resultatMax+1).fill(resultatMax+1).map((x,i)=>i).reverse();
+    } else {
+      // schüsse jeweils mit komma
+      return 0; // TODO: fix
+    }
+  }
+
+  getResultate(stich: Stich) {
+    let resultatMax = stich.scheibe*stich.anzahlschuss;
+    if (resultatMax % 10 == 0) {
+      // schüsse sind jeweils ganzzahlig
+      return Array(resultatMax+1).fill(resultatMax+1).map((x,i)=>i).reverse();
+    } else {
+      // schüsse jeweils mit komma
+      return 0; // TODO: fix
+    }
   }
 
 }
