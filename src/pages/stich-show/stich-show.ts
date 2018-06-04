96675f74a282c092d89b7cc5b0d4a081a41b0165
martinfrancois <fracoisma@hotmail.com>
2018-06-04 15:47:47 +0200

    implemented stich-show

--- src/pages/stich-show/stich-show.ts
+++ src/pages/stich-show/stich-show.ts
@@ -15,7 +15,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class StichShowPage {
 
+  stich;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.stich = navParams.get('stich');
   }
 
   ionViewDidLoad() {
