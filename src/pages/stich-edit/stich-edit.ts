d91b87a47e837bdee2ea5e0738933d332d234a05
Lukas Marchesi <lukas.marchesi@students.fhnw.ch>
2018-06-06 16:33:14 +0200

    fixed pushing with schuetzenfest

--- src/pages/stich-edit/stich-edit.ts
+++ src/pages/stich-edit/stich-edit.ts
@@ -22,9 +22,11 @@ export class StichEditPage {
   name: string;
   scheibe: number;
   anzahlSchuss: number;
+  schuetzenfestKey: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public fbSvc : FirebaseServiceProvider) {
     this.stich = navParams.get('stich');
+    this.schuetzenfestKey = navParams.get('schuetzenfestKey');
   }
 
   ionViewDidLoad() {
@@ -35,7 +37,7 @@ export class StichEditPage {
     this.stich.name = this.name;
     this.stich.scheibe = this.scheibe;
     this.stich.anzahlschuss = this.anzahlSchuss;
-    this.fbSvc.crudStich(this.stich, CRUD.UPDATE);
+    this.fbSvc.crudStich(this.stich, this.schuetzenfestKey, CRUD.UPDATE);
 
     // change view
     this.navCtrl.pop();
